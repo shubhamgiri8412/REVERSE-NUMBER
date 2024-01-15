15aa48aafd53937568307bfc86e77b8222e0ed7c
shubhamgiri8412 <156566481+shubhamgiri8412@users.noreply.github.com>
2024-01-15 20:00:09 +0530

    Add files via upload

---
+++ JA.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+public class JA {
+    public static void main(String[] args) {
+
+        int[] marks = {21, 32, 43, 65};
+        int sum = 0;
+
+        for (int elements:marks ) {
+            sum = sum+elements;
+
+        }
+        {
+
+            System.out.println("the value=" +sum );
+
+        }
+
+
+    }
+}
